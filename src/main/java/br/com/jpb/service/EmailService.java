8e06836294f22f620927030f81cb8b3ef08c2ae7
João Paulo Bassinello <joao.bassinello@tecsinapse.com.br>
2016-03-26 10:44:57 -0300

    adding SLF4J and some logs

--- src/main/java/br/com/jpb/service/EmailService.java
+++ src/main/java/br/com/jpb/service/EmailService.java
@@ -2,11 +2,13 @@ package br.com.jpb.service;
 
 import br.com.jpb.dao.GenericDao;
 import br.com.jpb.model.entity.Email;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Singleton;
@@ -26,7 +28,18 @@ import java.util.List;
 @Singleton
 public class EmailService {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
+
 	private static final int MAX_TRIES = 3;
+
+	@Value("${email.from}")
+	private String EMAIL_FROM;
+
+	@Value("${name.from}")
+	private String NAME_FROM;
+
+	@Value("${app.env}")
+	private String APP_ENV;
 
 	@Inject
 	private transient JavaMailSenderImpl mailSender;
@@ -37,15 +50,6 @@ public class EmailService {
 	@Inject
 	private transient Environment environment;
 
-	private String emailFrom;
-	private String nameFrom;
-
-	@PostConstruct
-	public void init() {
-		this.emailFrom = environment.getProperty("email.from");
-		this.nameFrom = environment.getProperty("name.from");
-	}
-
 	@Transactional
 	public void saveEmailAndSend(Email email) {
 		genericDao.persist(email);
@@ -55,8 +59,8 @@ public class EmailService {
 		final StringBuilder query = new StringBuilder();
 		query.append("select e from email ");
 		query.append("where e.sent = false and e.tries < :max");
-		return genericDao.getEm().createQuery(query.toString(), Email.class)
-				.setParameter("max", MAX_TRIES).getResultList();
+		return genericDao.getEm().createQuery(query.toString(), Email.class).setParameter("max",
+				MAX_TRIES).getResultList();
 	}
 
 	@Transactional
@@ -67,12 +71,15 @@ public class EmailService {
 
 	@Transactional
 	void send(Email email) throws MessagingException, IOException {
-		// TODO: block email send while not in production
-
+		LOGGER.info("Sending email {} to {}", email.getSubject(), email.getEmailTo());
+		if (!isProduction() && email.getEmailTo().endsWith("@tecsinapse.com.br")) {
+			markSent(email);
+			return;
+		}
 		MimeMessage message = mailSender.createMimeMessage();
-		message.setFrom(new InternetAddress(emailFrom, nameFrom));
-		message.addRecipients(Message.RecipientType.TO, new Address[]{new
-				InternetAddress(email.getEmailTo(), email.getNameTo())});
+		message.setFrom(new InternetAddress(EMAIL_FROM, NAME_FROM));
+		message.addRecipients(Message.RecipientType.TO, new Address[]{new InternetAddress(email
+				.getEmailTo(), email.getNameTo())});
 		message.setSubject(email.getSubject());
 
 		Multipart multipart = new MimeMultipart();
@@ -83,8 +90,16 @@ public class EmailService {
 
 		mailSender.send(message);
 
+		markSent(email);
+	}
+
+	private void markSent(Email email) {
 		email.setSentDateTime(new Date());
 		email.setSent(true);
 		genericDao.merge(email);
+	}
+
+	private boolean isProduction() {
+		return "Production".equals(APP_ENV);
 	}
 }
--- src/main/java/br/com/jpb/service/SendEmailJob.java
+++ src/main/java/br/com/jpb/service/SendEmailJob.java
@@ -1,6 +1,8 @@
 package br.com.jpb.service;
 
 import br.com.jpb.model.entity.Email;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 
 import javax.inject.Inject;
@@ -12,19 +14,26 @@ import java.util.List;
 @Singleton
 public class SendEmailJob {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(SendEmailJob.class);
+
 	@Inject
 	private transient EmailService emailService;
 
-	@Scheduled(fixedDelay = 1000)
+	@Scheduled(fixedDelay = 20_000)
 	public void sendEmails() {
 		final List<Email> toSend = emailService.findEmailsToSend();
+		LOGGER.info("Iniciando job para envio de emails. Quantidade de emails a enviar: " + toSend
+				.size());
 		for (Email email : toSend) {
 			try {
 				emailService.send(email);
 			} catch (Exception e) {
+				LOGGER.warn("Erro no job para envio de emails. Assunto do email: {}. " +
+						"Destinatário:" + " {}", email.getSubject(), email.getEmailTo());
 				emailService.updateTries(email);
 			}
 		}
+		LOGGER.info("Finalizando job para envio de emails.");
 	}
 
 }
